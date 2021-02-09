25c729bbe7f6fb02faf2cf223e5994f9012a37b6
manci <javiercaliescali@gmail.com>
2021-02-09 14:14:53 -0500

    -

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,7 +21,7 @@ module Api
       def update 
         user = User.find(params[:id])              
         begin
-          render json: UserSerializer.new(@user).serialized_json if user.update(user_params)
+          render json: UserSerializer.new(user).serialized_json if user.update(user_params)
         rescue ActiveRecord::RecordNotFound => e
           render json: { error: e.to_s }, status: :not_found 
         end
@@ -29,7 +29,6 @@ module Api
 
       def create
         user = User.new(user_params)
-
         if user.save
           render json: UserSerializer.new(user).serialized_json
         else
