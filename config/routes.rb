2614f752a1b3f47d32bb69cf23bf1b722fa7a374
manci <javiercaliescali@gmail.com>
2021-02-09 17:06:39 -0500

    test for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[index create destroy update show]
-      resources :categories, only: %i[index create destroy]
+      resources :categories, only: %i[index create destroy update]
       resources :products, only: %i[index create destroy update show]
     end
   end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CategoriesController, type: :controller do
   it { should route(:get, '/api/v1/categories').to(action: :index) }
+  it { should route(:post, '/api/v1/categories').to(action: :create) }
+  it { should route(:delete, '/api/v1/categories/1').to(action: :destroy, id: 1) }
+  it { should route(:put, '/api/v1/categories/1').to(action: :update, id: 1) }
   
 
   describe 'GET #index' do
